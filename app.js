df973e9a760387591b6988fe277b78446cd30b2a
Victor <adevic4christ@gmail.com>
2019-12-06 22:14:04 +0100

    add clearEditState

--- app.js
+++ app.js
@@ -81,6 +81,9 @@ const UICtrl = (function () {
 	const UISelectors = {
 		itemList: "#item-list",
 		addBtn: ".add-btn",
+		updateBtn: ".update-btn",
+		deleteBtn: ".delete-btn",
+		backBtn: ".back-btn",
 		itemNameInput: "#item-name",
 		itemCaloriesInput: "#item-calories",
 		totalCalories: ".total-calories"
@@ -133,6 +136,13 @@ const UICtrl = (function () {
 		showTotalCalories: function (totalCalories) {
 			document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
 		},
+		clearEditState: function () {
+			UICtrl.clearInput();
+			document.querySelector(UISelectors.updateBtn).style.display = "none";
+			document.querySelector(UISelectors.deleteBtn).style.display = "none";
+			document.querySelector(UISelectors.backBtn).style.display = "none";
+			document.querySelector(UISelectors.addBtn).style.display = "inline";
+		},
 		getSelectors: function () {
 			return UISelectors;
 		}
@@ -180,6 +190,8 @@ const App = (function (ItemCtrl, UICtrl) {
 	return {
 		init: function () {
 			console.log("initialising App...");
+			//Clear edit state / set inital state
+			UICtrl.clearEditState();
 
 			//fetch items from data structure
 			const items = ItemCtrl.getItems();
