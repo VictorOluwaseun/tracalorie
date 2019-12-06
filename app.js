2110f9e4c7e400c46c7f8a0485ff3b5b750a8821
Victor <adevic4christ@gmail.com>
2019-12-06 22:46:14 +0100

    add itemToEdit

--- app.js
+++ app.js
@@ -56,6 +56,17 @@ const ItemCtrl = (function () {
 
 			return newItem;
 		},
+		getItemById: function (id) {
+			let found = "";
+
+			//loop through items;
+			data.items.forEach(item => {
+				if (item.id === id) {
+					found = item;
+				}
+			});
+			return found;
+		},
 		getTotalCalories: function () {
 			let total = 0;
 
