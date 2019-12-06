1b8b9dbff9111381d1688d58553d079fc8dd6a1d
Victor <adevic4christ@gmail.com>
2019-12-06 23:03:03 +0100

    add showEditstate

--- app.js
+++ app.js
@@ -150,6 +150,7 @@ const UICtrl = (function () {
 		addItemToForm: function () {
 			document.querySelector(UISelectors.itemNameInput).value = ItemCtrl.getCurrentItem().name;
 			document.querySelector(UISelectors.itemCaloriesInput).value = ItemCtrl.getCurrentItem().calories;
+			UICtrl.showEditState();
 		},
 		hideList: function () {
 			document.querySelector(UISelectors.itemList).style.display = "none";
@@ -163,6 +164,12 @@ const UICtrl = (function () {
 			document.querySelector(UISelectors.deleteBtn).style.display = "none";
 			document.querySelector(UISelectors.backBtn).style.display = "none";
 			document.querySelector(UISelectors.addBtn).style.display = "inline";
+		},
+		showEditState: function () {
+			document.querySelector(UISelectors.updateBtn).style.display = "inline";
+			document.querySelector(UISelectors.deleteBtn).style.display = "inline";
+			document.querySelector(UISelectors.backBtn).style.display = "inline";
+			document.querySelector(UISelectors.addBtn).style.display = "none";
 		},
 		getSelectors: function () {
 			return UISelectors;
