903a2fa0e8b080d6dd8f8ab089f89c64b1818572
Victor <adevic4christ@gmail.com>
2019-12-06 19:54:37 +0100

    update structure

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // Storage Controller
 
 // Item Controller
+const ItemCtrl = (function () {
+
+})();
 
 // UI Controller
+const UICtrl = (function () {
+
+})();
 
 //App Controller
+const App = (function (ItemCtrl, UICtrl) {
+
+})(ItemCtrl, UICtrl);
